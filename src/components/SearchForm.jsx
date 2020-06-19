e4ab3a4ef8e5f0b6d006b0d36709c9663aeb0342
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-06-19 15:33:00 -0700

    Adding SearchForm and WoofHeader components

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SearchForm = () => (
+  <div className="search-form">
+    <input className="search-bar" type="text" />
+    <button className="search-button" type="submit">Search</button>
+  </div>
+);
+
+export default SearchForm;
---
+++ src/components/WoofHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header, Image } from 'semantic-ui-react';
+
+const WoofHeader = () => (
+  <Header as="h2">
+    <Image circular src="https://i.imgur.com/pBaQxiT.png" />
+    {' '}
+    Dulce Woof
+  </Header>
+);
+
+export default WoofHeader;
