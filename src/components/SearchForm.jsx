23d4d670cec2869130e87d32eb388f4d9d2e4c5d
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-06-22 13:53:31 -0700

    Adding config for env variables for Edamam API

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const SearchForm = () => (
   <div className="search-form">
-    <label htmlFor="search-bar">
+    <label htmlFor="search-bar" className="search-bar">
       <input
         id="search-bar"
-        className="search-bar"
+        className="search-bar expand"
         type="text"
         placeholder="Type an ingredient"
         label="Type an ingredient"
